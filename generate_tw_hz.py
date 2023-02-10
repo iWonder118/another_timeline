14222a9e51a57f3219ba4f3f9fd24511e8afc26a
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2023-02-11 00:09:36 +0900

    add max limit for base text

--- generate_tw_hz.py
+++ generate_tw_hz.py
@@ -3,12 +3,12 @@ import pandas as pd
 import markovify
 
 
-df = pd.read_csv("./result/tw_hz_no_lf.csv")
+df = pd.read_csv("./get_tw_hz/result/tw_hz_no_lf.csv")
 
 mecab = MeCab.Tagger("-Owakati")
 
 owakati_tweet = ""
-for tweet in df["text"]:
+for tweet in df.iloc[:9900, 2]:
     owakati_tweet += " ".join(mecab.parse(tweet).split()) + "\n"
 
 model = markovify.NewlineText(owakati_tweet)
